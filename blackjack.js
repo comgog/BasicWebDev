c7f537075c5fa063e83efc99ab3978188b54e606
bokrae <102729927+comgog@users.noreply.github.com>
2024-09-27 14:51:39 +0900

    week4: blackjack mod

--- blackjack.js
+++ blackjack.js
@@ -1,6 +1,6 @@
 // blackjack.js
-let cardOne = 7;
-let cardTwo = 5;
+let cardOne = 10;
+let cardTwo = 11;
 let userSum = cardOne + cardTwo;
 
 let cardOneBank = 7;
@@ -11,7 +11,7 @@ let bankSum = cardOneBank + cardTwoBank;
 if (userSum === 21) {
   if (bankSum === 21) {
     console.log("DRAW");
-  } else console.log("User win");
+  } else console.log("You have 21 points, blackjack win");
 }
 
 while (userSum !== 21 && bankSum !== 21) {
@@ -21,6 +21,9 @@ while (userSum !== 21 && bankSum !== 21) {
   userSum += cardThree;
   bankSum += cardThreeBank;
   console.log(`You have ${userSum} points, dealer has ${bankSum}`);
+  if (userSum === 21) {
+    console.log("You have 21 points, blackjack win");
+  }
 
   if (bankSum >= 17) {
     if (bankSum > 21 || (userSum <= 21 && userSum > bankSum)) {
